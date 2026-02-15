40a2d0791df8ebac675373e8542bf861a485a215
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-02-15 13:32:54 -0800

    Added token identification (changed approach from enumeration to help group understandings)

--- include/Tokens.h
+++ include/Tokens.h
@@ -1,15 +1,11 @@
 # pragma once       // prevents header being used more then once per .cpp file
 # include <string> 
 
-enum class TokenType    // token categories for lexer using enumeration
-{
-    Identifier,
-    Keyword,
-    Integer,
-    Real,
-    Operator,
-    Separator,
-    UnknownToken,
-    FileEnd,
-}; 
-
+const int T_Identifier = 1;   // These are the token idetification codes with int assignment
+const int T_Keyword = 2;
+const int T_Integer = 3;
+const int T_Real = 4;
+const int T_Operator = 5;
+const int T_Seperator = 6;
+const int T_UnknownTokenType = 7;
+const int T_FileEnd = 8;
