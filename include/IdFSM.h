e5dd0e3afa8268c2c316128bcf06bb6e1d55bc5e
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-02-15 13:56:59 -0800

    IDFSM implementation in include file

---
+++ include/IdFSM.h
@@ -0,0 +1,24 @@
+#pragma once            // prevents header being used more then once per .cpp file
+#include <string> 
+
+class IDFSM {
+
+public: 
+    IDFSM();    // below are the FSM's controls for implementation
+
+    void FSMstart();
+    bool makeCheck(char c);
+    bool validTokenAccept() const;
+    int startIdentifier(const std::string& s, int start); // looks for the start position
+
+
+    
+private:
+    static const int State_Start = 0;       // IDFSM states of opperation indicated by integer assign
+    static const int State_Identified = 1;
+    static const int State_End  = -1;
+    int state_ = State_Start;
+
+    bool isAletter_(char c) const;      // chekcs if a letter true or false
+    bool isAnumber_(char c) const;      // checks if a number true or false
+}
