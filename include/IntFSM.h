cca170d965c2f1a0b85070ecd04e47a5d7321a48
Andrew Bernabe <ajbernabe4@csu.fullerton.edu>
2026-02-23 15:16:08 -0800

    all tests  passed and updated some bugs

--- include/IntFSM.h
+++ include/IntFSM.h
@@ -12,7 +12,7 @@ int startInteger(const std::string& s, int start);
 
 private:
 static const int Start_State = 0;
-static const int Start_InInt = -1;
+static const int Start_InInt = 1;
 static const int State_End = -1;
 int state_ = Start_State;
 bool isAnumber_(char c) const;
