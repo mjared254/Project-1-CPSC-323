2ffe46fbe4480aa5d750499d89ff12913ae154a0
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-02-23 18:28:20 -0800

    keywords to load into lexer

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -20,7 +20,7 @@ Lexer::Lexer(const std::string& filename) { //LOAD the file into the source_
     line_ = 1;
 }
 
-// FILE CHEK INFORMATION
+// FILE CHEK INFORMATION section
 bool Lexer::isOpen() const {
     return fileOpened_;
 }
@@ -30,3 +30,25 @@ bool Lexer::isLetter_(char c) const{
 bool Lexer::isDigit_(char c) const{
     return (c >= '0' && c <= '9');
 }
+
+void Lexer::keywordload_()
+{
+    // THE KEYWORDS for Rat
+    keywords_.push_back("integer");
+    keywords_.push_back("if");
+    keywords_.push_back("otherwise");
+    keywords_.push_back("fi");
+    keywords_.push_back("while");
+    keywords_.push_back("return");
+    keywords_.push_back("read");
+    keywords_.push_back("write");
+    keywords_.push_back("function");
+    keywords_.push_back("boolean");
+    keywords_.push_back("real");
+    keywords_.push_back("true");
+    keywords_.push_back("false");
+}
+
+
+
+
