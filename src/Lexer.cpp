8666768ca718d9f9fcd99882b236a88f44bbf886
Matthew Lopez <matthewlopez73@csu.fullerton.edu>
2026-02-23 18:11:32 -0800

    file loading for _source

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -3,3 +3,19 @@
 #include <fstream>
 #include <iterator>
 
+Lexer::Lexer(const std::string& filename) { //LOAD the file into the source_
+    keywordload_();
+
+    std::ifstream inFile(filename);
+    if (!inFile.is_open()) {
+        fileOpened_ = false;
+        return;
+    }
+
+    fileOpened_ = true;
+    source_.assign(std::istreambuf_iterator<char>(inFile),
+                   std::istreambuf_iterator<char>());
+
+    index_ = 0;
+    line_ = 1;
+}
